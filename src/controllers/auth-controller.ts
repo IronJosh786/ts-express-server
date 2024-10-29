4d06403ba1cade212b7f71884a54d0838a7a7135
Faizan <sfaizan78692@gmail.com>
2024-10-29 19:27:01 +0530

    make cookies to be strict

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -45,7 +45,7 @@ export const signUp = asyncHandler(async (req: Request, res: Response) => {
     res.cookie("access_token", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "strict",
       maxAge: cookieExpiryTimeInMS,
     });
 
@@ -95,7 +95,7 @@ export const signIn = asyncHandler(async (req: Request, res: Response) => {
     res.cookie("access_token", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "strict",
       maxAge: cookieExpiryTimeInMS,
     });
 
@@ -155,7 +155,7 @@ export const refreshToken = asyncHandler(
       res.cookie("access_token", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "strict",
         maxAge: cookieExpiryTimeInMS,
       });
 
@@ -196,7 +196,7 @@ export const logout = asyncHandler(async (req: Request, res: Response) => {
     res.clearCookie("access_token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "strict",
       maxAge: 0,
     });
 
